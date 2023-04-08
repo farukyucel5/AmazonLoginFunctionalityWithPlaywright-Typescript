a723fb1a9de128d7aca0eefaaeceab583b110dcf
farukyucel5 <farukyucel5@gmail.com>
2023-04-08 20:51:22 +0300

    login functionality

--- tests/loginFunctionality.spec.ts
+++ tests/loginFunctionality.spec.ts
@@ -1,25 +1,47 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-    await page.goto('https://www.amazon.com/');
-    const locator = page.getByRole('link', { name: 'Hello, sign in' })
-    await locator.hover()
-    const signUpButton =page.locator("(//span[text()='Sign in'])[1]")
-    await signUpButton.click()
-   
-  });
-
-
-
-
 test.describe("login", ()=>{
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://www.amazon.com/');
+        const locator = page.getByRole('link', { name: 'Hello, sign in' })
+        await locator.hover()
+        const signUpButton =page.locator("(//span[text()='Sign in'])[1]")
+        await signUpButton.click()
+       
+      });
+    
 
     test('invalid email test', async ({ page }) => {
         const emailBox=page.locator("//input[@id='ap_email']")
         await emailBox.fill("gmail.com")
         await page.locator("//input[@id='continue']").click()
         await expect(page.getByRole('heading',{name:'There was a problem'})).toBeVisible()
-        page.close()
+    });
+
+    test('null email test', async({page})=>{
+        const emailBox=page.locator("//input[@id='ap_email']")
+        await page.locator("//input[@id='continue']").click()
+        await expect(page.getByRole('heading',{name:'There was a problem'})).toBeVisible()
+        //await expect(page.locator("//div[@id='auth-email-missing-alert']")).toBeVisible()
+
+    });
+
+    test("invalid password", async({page})=>{
+        const emailBox=page.locator("//input[@id='ap_email']")
+        await emailBox.fill("f@gmail.com")
+        await page.locator("//input[@id='continue']").click()
+        const passwordBox=page.locator("//input[@id='ap_password']")
+        await passwordBox.fill("123456")
+        await page.locator("//input[@id='signInSubmit']").click()
+        await expect(page.getByRole('heading',{name:'Important Message!'})).toBeVisible()
+
+})
+
+
+    test.afterEach(async ({page})=>{
+        
+      page.close()
+
     });
 
 
